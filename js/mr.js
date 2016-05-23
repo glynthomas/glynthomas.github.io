8f7bd9e1299b310f1de8e054ebdf46ba669ceb49
glyn thomas <glynthoma@gmail.com>
2016-05-23 20:32:05 +0100

    update

--- js/mr.js
+++ js/mr.js
@@ -493,6 +493,7 @@ document.onclick = function keyClick(event) {
    case (tagid === 'body'):
    case (tagid === 'menu'):
    case (tagid === 'wrapper'):
+   case (tagid === 'overlay'):
    case (tagid === 'nav-toggle'):
    case (tagclass === 'mobile-menu-trigger'):
 
