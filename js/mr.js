3059ef77053b61b5162c273d4681503223e2a031
glyn thomas <glynthoma@gmail.com>
2016-05-24 19:37:32 +0100

    update

--- js/mr.js
+++ js/mr.js
@@ -41,10 +41,6 @@ window.contactstate = 0;
 window.overlaystate = 0;
 window.scrollstate = 0;
 
-if (window.innerHeight > window.innerWidth){
-    document.getElementsByTagName("body").style.transform = "rotate(90deg)";
-}
-
 
 function _process(ctype, cwho, cdata, cpath) {
 
